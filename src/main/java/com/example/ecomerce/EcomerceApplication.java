f2b95574c6c51b20bc35b91e508c3ac696a5bea7
dramirez <david.ramirez@tata.com.uy>
2025-02-20 23:08:48 -0300

    complete application.yml config, add banner and scripts sql

--- src/main/java/com/example/ecomerce/EcomerceApplication.java
+++ src/main/java/com/example/ecomerce/EcomerceApplication.java
@@ -1,9 +1,14 @@
 package com.example.ecomerce;
 
+import com.example.ecomerce.common.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(Constants.NAMESPACES)
+@EnableJpaRepositories(Constants.NAMESPACES)
 public class EcomerceApplication {
 
     public static void main(String[] args) {
